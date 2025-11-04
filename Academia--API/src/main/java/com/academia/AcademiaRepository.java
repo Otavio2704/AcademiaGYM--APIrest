5ed20b92b1530ea5cde52f4ed5603c62f3e8f33e
Otavio2704 <otavioaredes62@gmail.com>
2025-11-04 18:39:43 -0300

    Coloquei o repositorie da academia no local errado

--- Academia--API/src/main/java/com/academia/AcademiaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.academia.repository;
-
-import com.academia.model.Academia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AcademiaRepository extends JpaRepository<Academia, Long> {}
