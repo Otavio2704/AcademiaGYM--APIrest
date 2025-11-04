4662353da818cb9919e399455e082c5841750f50
Otavio2704 <otavioaredes62@gmail.com>
2025-11-04 18:30:29 -0300

    Delete Academia--API directory

--- Academia--API/src/main/java/com/academia/repository/InstrutorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.academia.repository;
-
-import com.academia.model.Instrutor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstrutorRepository extends JpaRepository<Instrutor, Long> {}
--- Academia--API/src/main/java/com/academia/repository/TreinoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.academia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.academia.model.Treino;
-
-public interface TreinoRepository extends JpaRepository<Treino, Long> {
-}
